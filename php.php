246f3b49e305172e1644d4e0896d1561883a4417
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-17 13:24:19 +0700

    Create php.php

---
+++ php.php
@@ -0,0 +1,8 @@
+#Command php on ubuntu
+
+$ sudo add-apt-repository ppa:ondrej/php
+$ sudo apt install php(version)
+$ sudo apt install php7.4-(ektensi)
+$ sudo a2enmod php(version)
+$ sudo update-alternatives --set php /usr/bin/php7.4
+$ Sudo systemctl restart php
