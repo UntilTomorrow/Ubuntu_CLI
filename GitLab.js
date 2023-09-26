24f6e5d0887c1aa8bacf4179297e449fc6ebf022
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-09-26 15:04:41 +0700

    Update GitLab.js

--- GitLab.js
+++ GitLab.js
@@ -8,9 +8,28 @@ $ sudo apt-get install gitlab-ce
 
 $ sudo nano /etc/gitlab/gitlab.rb
   -> ubah external_url 'https://example.com' dengan domain atau IP Server 
-$ sudo gotlab-ctl status
+$ sudo gitlab-ctl reconfigure
+$ sudo gitlab-ctl status
+
+#optional reset password root 
+$ sudo gitlab-rake "gitlab:password:reset"
+$ sudo nano /etc/gitlab/initial_root_password
 
 $ git config --global user.name "name"
 $ git config --global user.email "email@example.com"
 
+#tambah git remote di direktori yang akan di push / pull
+
+$ git init
+$ git remote -v
+$ git pull origin (namabranch)
+
+$ git checkout -b (branch)
+rebase 
+$ git pull --rebase origin(branch)
+
+$ touch file 
+$ git add .
+$ git commit-m "push puh"
+$ git push origin -u (branch)
 
