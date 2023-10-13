a76739ca984be1aca47d5821553f6cd6849ad61b
Heru Cahyo <113245217+UntilTomorrow@users.noreply.github.com>
2023-10-13 15:51:17 +0700

    Update php.php

--- php.php
+++ php.php
@@ -6,3 +6,7 @@ $ sudo apt install php7.4-(ektensi)
 $ sudo a2enmod php(version)
 $ sudo update-alternatives --set php /usr/bin/php7.4
 $ Sudo systemctl restart php
+
+
+# Windows change PHP 
+set PATH=C:\Program Files\Ampps\php74;%PATH%
